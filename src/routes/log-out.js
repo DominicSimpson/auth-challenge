baa99227298325485e20a8048289985b5d50b431
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-01-29 18:12:36 +0000

    Attempt at Challenge Four

--- src/routes/log-out.js
+++ src/routes/log-out.js
@@ -1,8 +1,11 @@
 const { session_id } = require("../routes/sign-up.js");
+const { removeSession } = require("../model/session.js");
 
 function post(req, res) {
   const sid = req.signedCookies.sid;
   removeSession(sid);
+  response.clearCookie(sid);
+  res.redirect(`/home`);
 
   /**
    * [1] Get the session ID from the cookie
